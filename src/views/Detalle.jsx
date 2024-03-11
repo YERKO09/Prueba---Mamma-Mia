e715de5e866a95863d4284ec93d69bf4809373de
Yerko Browton <yerko.browton1995@hotmail.com>
2024-03-11 17:09:36 -0300

    primer commit

---
+++ src/views/Detalle.jsx
@@ -0,0 +1,14 @@
+import PizzaCard from "../components/PizzaCard";
+
+const Detalle = () => {
+
+  return (
+    <>
+      <div className="container">
+        <PizzaCard />
+      </div>
+    </>
+  );
+};
+
+export default Detalle;
